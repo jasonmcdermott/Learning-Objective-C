97ddab13dd0a3e6de651e84ce6f54407273e7fa8
Jason Macbook Air <jason@jasonmcdermott.net>
2014-01-11 18:12:06 +1100

    Apple sample code for BLE device interaction

---
+++ Apple Sample Code/HeartMonitor/HeartMonitor/HRMViewController.h
@@ -0,0 +1,13 @@
+//
+//  HRMViewController.h
+//  HeartMonitor
+//
+//  Created by Main Account on 12/13/13.
+//  Copyright (c) 2013 Razeware LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HRMViewController : UIViewController
+
+@end
