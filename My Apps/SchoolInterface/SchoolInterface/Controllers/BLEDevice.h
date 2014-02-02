abdeafea3743ac90d891bc760c9f3e3f77443035
Jason Macbook Air <jason@jasonmcdermott.net>
2014-02-03 02:04:29 +1100

    app now receives BTLE data from the MIO Alpha. Yet to build working user interface to allow selection of specific device

--- My Apps/SchoolInterface/SchoolInterface/Controllers/BLEDevice.h
+++ My Apps/SchoolInterface/SchoolInterface/Controllers/BLEDevice.h
@@ -13,10 +13,19 @@
 #import "SENPDDriver.h"
 #import "SENXmlDataGenerator.h"
 
+#import <CoreBluetooth/CoreBluetooth.h>
+
 #import "RFduinoManager.h"
 #import "RFduino.h"
 
 #import "SENUtilities.h"
+
+#define HRM_DEVICE_INFO_SERVICE_UUID @"180A"       // 180A = Device Information
+#define HRM_HEART_RATE_SERVICE_UUID @"180D"        // 180D = Heart Rate Service
+#define HRM_ENABLE_SERVICE_UUID @"2A39"
+#define HRM_NOTIFICATIONS_SERVICE_UUID @"2A37"
+#define HRM_BODY_LOCATION_UUID @"2A38"
+#define HRM_MANUFACTURER_NAME_UUID @"2A29"
 
 @class RFduinoManager;
 @class RFduino;
@@ -34,7 +43,7 @@ typedef struct
 
 @end
 
-@interface BLEDevice : UIViewController <BLEDelegate, RFduinoManagerDelegate, RFduinoDelegate>
+@interface BLEDevice : UIViewController <BLEDelegate, RFduinoManagerDelegate, RFduinoDelegate, CBCentralManagerDelegate, CBPeripheralDelegate>
 {
     int counter;
     volatile unsigned _bufferIndex;
@@ -44,6 +53,28 @@ typedef struct
     volatile bool _started;
     volatile bool _reliable;
 }
+
+@property (nonatomic, strong) CBCentralManager *centralManager;
+@property (nonatomic, strong) CBPeripheral     *heartRateMonitorPeripheral;
+
+// Properties to hold data characteristics for the peripheral device
+@property (nonatomic, strong) NSString   *BTLEconnected;
+@property (nonatomic, strong) NSString   *BTLEbodyData;
+@property (nonatomic, strong) NSString   *BTLEmanufacturer;
+@property (nonatomic, strong) NSString   *BTLEdeviceData;
+@property (assign) uint16_t BTLEheartRate;
+
+// Properties to handle storing the BPM and heart beat
+@property (nonatomic, strong) UILabel    *heartRateBPM;
+@property (nonatomic, retain) NSTimer    *pulseTimer;
+
+// Instance method to get the heart rate BPM information
+- (void) getHeartBPMData:(CBCharacteristic *)characteristic error:(NSError *)error;
+
+// Instance methods to grab device Manufacturer Name, Body Location
+- (void) getManufacturerName:(CBCharacteristic *)characteristic;
+- (void) getBodyLocation:(CBCharacteristic *)characteristic;
+
 
 @property (nonatomic, weak) id <BLEDeviceDelegate> delegate;
 @property(strong, nonatomic) RFduino *connected_rfduino;
--- My Apps/SchoolInterface/SchoolInterface/Controllers/BLEDevice.m
+++ My Apps/SchoolInterface/SchoolInterface/Controllers/BLEDevice.m
@@ -39,8 +39,6 @@ NSString * const  USERNAME_KEY = @"BrightheartsUsername";
 @property (strong, nonatomic) NSMutableArray *tempDevices;
 @end
 
-
-
 @implementation BLEDevice
 
 #pragma mark - Init
@@ -63,6 +61,8 @@ NSString * const  USERNAME_KEY = @"BrightheartsUsername";
     NSLog(@"RBLMainViewController didLoad");
     self.passedToParent = NO;
     
+    self.BTLEdeviceData = nil;
+    
     self.bleShield = [[BLE alloc] init];
     [self.bleShield controlSetup];
     self.bleShield.delegate = self;
@@ -74,7 +74,6 @@ NSString * const  USERNAME_KEY = @"BrightheartsUsername";
     self.mSesionData = [[SENSessionData alloc] init];
     self.mDevices = [[NSMutableArray alloc] init];
     self.tempDevices = [[NSMutableArray alloc] init];
-    
 
     _max_inactivity = DEF_MAX_INACTIVITY;
     
@@ -185,6 +184,7 @@ NSString * const  USERNAME_KEY = @"BrightheartsUsername";
     [self.bleShield findBLEPeripherals:3];
     [self.rfduinoManager startScan];
     self.disconnected = NO;
+    [self scanForBTLEDevices];
     [NSTimer scheduledTimerWithTimeInterval:(float)3.0 target:self selector:@selector(connectionTimer:) userInfo:nil repeats:NO];
     self.isFindingLast ? [self showButtons:@"connectPrevious"] : [self showButtons:@"scanDevices"];
 }
@@ -227,7 +227,193 @@ NSString * const  USERNAME_KEY = @"BrightheartsUsername";
     [self.delegate startGLView];
 }
 
-#pragma mark - Bluetooth Periphery
+#pragma mark - BTLE Utilities
+
+- (void)scanForBTLEDevices
+{
+    NSLog(@"---- Started scan");
+    NSArray *services = @[[CBUUID UUIDWithString:HRM_HEART_RATE_SERVICE_UUID], [CBUUID UUIDWithString:HRM_DEVICE_INFO_SERVICE_UUID]];
+	CBCentralManager *centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
+	[centralManager scanForPeripheralsWithServices:services options:nil];
+	self.centralManager = centralManager;
+}
+
+// method called whenever the device state changes.
+- (void)centralManagerDidUpdateState:(CBCentralManager *)central
+{
+    NSLog(@"---- device state changed");
+	// Determine the state of the peripheral
+	if ([central state] == CBCentralManagerStatePoweredOff) {
+		NSLog(@"CoreBluetooth BLE hardware is powered off");
+	}
+	else if ([central state] == CBCentralManagerStatePoweredOn) {
+		NSLog(@"CoreBluetooth BLE hardware is powered on and ready");
+	}
+	else if ([central state] == CBCentralManagerStateUnauthorized) {
+		NSLog(@"CoreBluetooth BLE state is unauthorized");
+	}
+	else if ([central state] == CBCentralManagerStateUnknown) {
+		NSLog(@"CoreBluetooth BLE state is unknown");
+	}
+	else if ([central state] == CBCentralManagerStateUnsupported) {
+		NSLog(@"CoreBluetooth BLE hardware is unsupported on this platform");
+	}
+}
+
+// method called whenever we have successfully connected to the BLE peripheral
+- (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral
+{
+    NSLog(@"---- Connected");
+	[peripheral setDelegate:self];
+    [peripheral discoverServices:nil];
+	self.BTLEconnected = [NSString stringWithFormat:@"Connected: %@", peripheral.state == CBPeripheralStateConnected ? @"YES" : @"NO"];
+}
+
+// CBPeripheralDelegate - Invoked when you discover the peripheral's available services.
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error
+{
+    NSLog(@"---- didDiscoverServices");
+	for (CBService *service in peripheral.services) {
+		[peripheral discoverCharacteristics:nil forService:service];
+	}
+}
+
+// CBCentralManagerDelegate - This is called with the CBPeripheral class as its main input parameter. This contains most of the information there is to know about a BLE peripheral.
+- (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI
+{
+    NSLog(@"---- didDiscoverPeripheral");
+	NSString *localName = [advertisementData objectForKey:CBAdvertisementDataLocalNameKey];
+	if (![localName isEqual:@""]) {
+        NSLog(@"%@",localName);
+		// We found the Heart Rate Monitor
+        /* need to do the following;
+        
+         1. populate a list of peripheral objects, let's call it BTLEPeripherals
+         2. add this device to our mDevices array, as well as our NSMutableDict
+         3. after a time period, stop BTLE scan
+         4. present full list to user
+         5. when user selects item from our list, hook that up to this BTLEPeripherals array
+         6. also allow user to disconnect.
+         
+        */
+		[self.centralManager stopScan];
+		self.heartRateMonitorPeripheral = peripheral;
+		peripheral.delegate = self;
+		[self.centralManager connectPeripheral:peripheral options:nil];
+	}
+}
+
+// Invoked when you discover the characteristics of a specified service.
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(NSError *)error
+{
+    NSLog(@"---- didDiscoverCharacteristicsForServices");
+	if ([service.UUID isEqual:[CBUUID UUIDWithString:HRM_HEART_RATE_SERVICE_UUID]])  {  // 1
+		for (CBCharacteristic *aChar in service.characteristics)
+		{
+			// Request heart rate notifications
+			if ([aChar.UUID isEqual:[CBUUID UUIDWithString:HRM_NOTIFICATIONS_SERVICE_UUID]]) { // 2
+				[self.heartRateMonitorPeripheral setNotifyValue:YES forCharacteristic:aChar];
+			}
+			// Request body sensor location
+			else if ([aChar.UUID isEqual:[CBUUID UUIDWithString:HRM_BODY_LOCATION_UUID]]) { // 3
+				[self.heartRateMonitorPeripheral readValueForCharacteristic:aChar];
+			}
+            //			else if ([aChar.UUID isEqual:[CBUUID UUIDWithString:POLARH7_HRM_ENABLE_SERVICE_UUID]]) { // 4
+            //				// Read the value of the heart rate sensor
+            //				UInt8 value = 0x01;
+            //				NSData *data = [NSData dataWithBytes:&value length:sizeof(value)];
+            //				[peripheral writeValue:data forCharacteristic:aChar type:CBCharacteristicWriteWithResponse];
+            //			}
+		}
+	}
+	// Retrieve Device Information Services for the Manufacturer Name
+	if ([service.UUID isEqual:[CBUUID UUIDWithString:HRM_DEVICE_INFO_SERVICE_UUID]])  { // 5
+        for (CBCharacteristic *aChar in service.characteristics)
+        {
+            if ([aChar.UUID isEqual:[CBUUID UUIDWithString:HRM_MANUFACTURER_NAME_UUID]]) {
+                [self.heartRateMonitorPeripheral readValueForCharacteristic:aChar];
+                NSLog(@"Found a Device Manufacturer Name Characteristic");
+            }
+        }
+	}
+}
+
+// Invoked when you retrieve a specified characteristic's value, or when the peripheral device notifies your app that the characteristic's value has changed.
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+{
+    NSLog(@"---- didUpdateValueForCharacteristics");
+	// Updated value for heart rate measurement received
+	if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:HRM_NOTIFICATIONS_SERVICE_UUID]]) { // 1
+		// Get the Heart Rate Monitor BPM
+		[self getHeartBPMData:characteristic error:error];
+	}
+	// Retrieve the characteristic value for manufacturer name received
+    if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:HRM_MANUFACTURER_NAME_UUID]]) {  // 2
+		[self getManufacturerName:characteristic];
+    }
+	// Retrieve the characteristic value for the body sensor location received
+	else if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:HRM_BODY_LOCATION_UUID]]) {  // 3
+		[self getBodyLocation:characteristic];
+    }
+	
+	// Add our constructed device information to our UITextView
+    //	self.BTLEdeviceInfo.text = [NSString stringWithFormat:@"%@\n%@\n%@\n", self.connected, self.bodyData, self.manufacturer];  // 4
+}
+
+// Instance method to get the heart rate BPM information
+- (void) getHeartBPMData:(CBCharacteristic *)characteristic error:(NSError *)error
+{
+    NSLog(@"---- getHeartBPMData");
+	// Get the Heart Rate Monitor BPM
+	NSData *data = [characteristic value];      // 1
+	const uint8_t *reportData = [data bytes];
+	uint16_t bpm = 0;
+	
+	if ((reportData[0] & 0x01) == 0) {          // 2
+		// Retrieve the BPM value for the Heart Rate Monitor
+		bpm = reportData[1];
+	}
+	else {
+		bpm = CFSwapInt16LittleToHost(*(uint16_t *)(&reportData[1]));  // 3
+	}
+	// Display the heart rate value to the UI if no error occurred
+	if( (characteristic.value)  || !error ) {   // 4
+		self.BTLEheartRate = bpm;
+        NSLog(@"%hu",self.BTLEheartRate);
+        //		self.heartRateBPM.text = [NSString stringWithFormat:@"%i bpm", bpm];
+        //		self.heartRateBPM.font = [UIFont fontWithName:@"Futura-CondensedMedium" size:28];
+        //		[self doHeartBeat];
+        //		self.pulseTimer = [NSTimer scheduledTimerWithTimeInterval:(60. / self.heartRate) target:self selector:@selector(doHeartBeat) userInfo:nil repeats:NO];
+	}
+	return;
+}
+
+// Instance method to get the manufacturer name of the device
+- (void) getManufacturerName:(CBCharacteristic *)characteristic
+{
+    NSLog(@"---- getManName");
+	NSString *manufacturerName = [[NSString alloc] initWithData:characteristic.value encoding:NSUTF8StringEncoding];
+	self.BTLEmanufacturer = [NSString stringWithFormat:@"Manufacturer: %@", manufacturerName];
+	return;
+}
+
+// Instance method to get the body location of the device
+- (void) getBodyLocation:(CBCharacteristic *)characteristic
+{
+    NSLog(@"---- getBodyLoc");
+	NSData *sensorData = [characteristic value];
+	uint8_t *bodyData = (uint8_t *)[sensorData bytes];
+	if (bodyData ) {
+		uint8_t bodyLocation = bodyData[0];
+		self.BTLEbodyData = [NSString stringWithFormat:@"Body Location: %@", bodyLocation == 1 ? @"Chest" : @"Undefined"];
+	}
+	else {
+		self.BTLEbodyData = [NSString stringWithFormat:@"Body Location: N/A"];
+	}
+	return;
+}
+
+#pragma mark - Redbear & RFduino Periphery
 
 - (void)disconnectPeripheralsBeforeScanning
 {
