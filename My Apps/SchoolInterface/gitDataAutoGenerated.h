75089865006dfaeb11a847a0d605f28f9f0b05c7
Jason Macbook Air <jason@jasonmcdermott.net>
2014-02-12 21:01:09 +1100

    adding in build version script & file

---
+++ My Apps/SchoolInterface/gitDataAutoGenerated.h
@@ -0,0 +1,11 @@
+//-----------------------------------------
+// Auto generated file
+// Created 2014-02-12 20:58:56
+//-----------------------------------------
+
+#define BUILD_DATE              @"2014-02-12 20:58:56"
+#define GIT_CURRENT_BRANCH      @"commercial-devices-only"
+#define GIT_LAST_COMMIT_HASH    @"be22b86"
+#define GIT_LAST_COMMIT_DATE    @"2014-02-05"
+#define GIT_LAST_COMMIT_COMMENT @"fluxtream iOS connection app. amazing!"
+#define GIT_LAST_REPO_TAG       @"0.15"
