cb564b2d802474daf14d533264a5510112e85dd5
Jason Macbook Air <jason@jasonmcdermott.net>
2014-02-12 21:06:20 +1100

    shifting development to new app directory.

--- Apps/SchoolInterface/SchoolInterface/gitDataAutoGenerated.h
+++ Apps/SchoolInterface/SchoolInterface/gitDataAutoGenerated.h
@@ -1,9 +1,9 @@
 //-----------------------------------------
 // Auto generated file
-// Created 2014-02-12 21:03:57
+// Created 2014-02-12 21:03:01
 //-----------------------------------------
 
-#define BUILD_DATE              @"2014-02-12 21:03:57"
+#define BUILD_DATE              @"2014-02-12 21:03:01"
 #define GIT_CURRENT_BRANCH      @"commercial-devices-only"
 #define GIT_LAST_COMMIT_HASH    @"bf385e3"
 #define GIT_LAST_COMMIT_DATE    @"2014-02-12"
